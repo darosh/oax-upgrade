2b97d6f97c9dade8daddfa5e7b0c190b419447a4
Jan Forst <ensonador@gmail.com>
2018-12-05 12:23:23 +0100

    schema data package

--- packages/data-schema/cli.js
+++ packages/data-schema/cli.js
@@ -1,10 +1,15 @@
+#!/usr/bin/env node
+
 const schemaBundler = require('json-schema-bundler')
 const yaml = require('yaml-js')
 const axios = require('axios')
 const circularJson = require('circular-json')
 const compactJson = require('json-stringify-pretty-compact')
-const fs = require('fs')
-const { URL } = require('url');
+const { writeFileSync } = require('fs')
+const { URL } = require('url')
+
+const TARGET = process.argv[2]
+
 global.URL = URL
 
 const url = 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/schemas/v2.0/schema.json'
@@ -14,5 +19,5 @@ schema.load().then(() => {
   schema.bundle()
   schema.deref(true, true)
   delete schema.bundled.definitions['http:']
-  fs.writeFileSync('assets/data/swagger-schema.json', compactJson(JSON.parse(circularJson.stringify(schema.bundled)), {maxLength: 120}))
+  writeFileSync(TARGET, compactJson(JSON.parse(circularJson.stringify(schema.bundled)), {maxLength: 120}))
 }).catch(err => console.log(err))
